9fa8622fbdf51934e0fad51c0c1a7ae85b4b8560
Stephen J. Carnam <steve@steveorevo.com>
2016-03-11 15:48:51 -0800

    Platform independent.

--- platform/win32/boot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM
-REM Boot native CLI
-REM
-SET PATH=c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;c:\xampplite\ds-plugins\dev-cli\library\platform\win32\cygwin\bin;%PATH%
-%*
