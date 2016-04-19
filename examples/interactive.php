aa66ca2ca52b402b2a27a0506610dbacafb400cb
Stephen J. Carnam <steve@steveorevo.com>
2016-04-18 22:35:13 -0700

    Interactive example checked in.

--- examples/interactive.php
+++ examples/interactive.php
@@ -1,37 +1,44 @@
 <?php
 /**
- * TO DO: lftp or ssh example of waiting, pressing 'y', and callback conditional.
+ * Interactively create a text file and then delete it by typing 'y' at the command line interface. Here we will start
+ * executing commands right away and use callbacks to interactively respond to the prompts.
  *
- * To use this example, move this file above vendor.
  */
+require('../vendor/autoload.php');
+include('../src/Steveorevo/VirtualCLI/VirtualCLI.php');
+use Steveorevo\VirtualCLI\VCLIManager;
+use Steveorevo\VirtualCLI\VirtualCLI;
 
-//
-///**
-// * Example...
-// */
-////$myVCLI = [];
-////for ($y = 0; $y < 10; $y++) {
-////	$myVCLI[$y] = new VirtualCLI("Test " . $y);
-////	for ($x = 0; $x < 10; $x++) {
-////		$msg = "echo $y$x";
-////		$myVCLI[$y]->add_command($msg);
-////	}
-////	echo $myVCLI[$y]->get_results();
-////}
-//global $myVCLI;
-//$myVCLI = new VirtualCLI("Test", 10, 15);
-//$myVCLI->add_command("echo Hello");
-//$myVCLI->get_results(function($results){
-//	echo $results;
-//	global $myVCLI;
-//	$myVCLI->add_command("echo mars");
-//	$myVCLI->get_results(function($results){
-//		echo $results;
-//		global $myVCLI;
-//		$myVCLI->closeAll();
-//	});
-//});
-////$myVCLI->add_command("echo Goodbye");
-////$myVCLI->get_results(function($results){
-////	echo $results;
-////});
+// Create a new virtual command line interface named "test"
+$myVCLI = new VirtualCLI("test");
+
+// Start processing commands as they come in, interactively
+$myVCLI->start();
+
+// Echo 'hello world' to a file in the current directory
+$myVCLI->add_command('echo Hello World > test.txt');
+
+// Remove the file interactively by waiting for the Y/n prompt
+if (VCLIManager::$platform === 'win32') {
+	$command = 'del /P test.txt';
+}else{
+	$command = 'rm -i test.txt';
+}
+
+// Wait for the question mark to appear and invoke the callback
+$myVCLI->add_command($command, '?', function($results) use ($myVCLI) {
+
+	// Send a Y for 'yes' and wait a second
+	$myVCLI->add_command('Y', 1);
+});
+
+// Wait until the queue is done
+while(false === $myVCLI->is_done()) {
+	usleep(1000); // Wait a second
+}
+
+// Show the complete prompt history
+echo $myVCLI->get_results();
+
+// Automatically close all terminals and shutdown the VCLI daemon
+VCLIManager::shutdown();
